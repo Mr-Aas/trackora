0efdc60350510cb2d719a861371fa32008fc21e7
aas solis <mraas0003@gmail.com>
2026-08-03 14:32:39 +0530

    initial structure of clientside

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png
+import Taskbar from './components/taskbar'
+import Header from './components/header'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header  />
+    <Taskbar />
+      {/* <div className="main bg-red-400">hello</div> */}
+
+    </>
+  )
+}
+
+export default App
